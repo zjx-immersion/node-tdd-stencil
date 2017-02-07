e9b3883a9a6e3be2f15fd291c534fcd416891e13
zjx-immersion <jianxin_home@163.com>
2017-02-07 16:29:03 +0800

    [Jason] this branch is used to test new js feature and packages

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 var main = require("./src/main.js")
 
 console.log(main());
+
+var products = [
+    { name: 'Running shoes', price: 75 },
+    { name: 'Golf shoes', price: 85 },
+    { name: 'Dress shoes', price: 95 },
+    { name: 'Walking shoes', price: 65 },
+    { name: 'Sandals', price: 55 }
+];
+
+for (var i = 0; i < products.length; i++) {
+    console.log(products[i]);
+}
+
+console.log("---------------------------");
+
+products.forEach((product, index) => {
+    console.log(product);
+});
+
+console.log("---------------------------");
+
+//yield
+function* myGenerator() {
+    console.log(1);
+    let a = yield 'first yield';
+    console.log(a); // 2
+    let b = yield 'second yield';
+    console.log(b); // 3
+    return 'hi';
+}
+
+let iterator = myGenerator();
+let firstYield = iterator.next(); // { value: 'first yield', done: false }
+let secondYield = iterator.next(2); // { value: 'second yield', done: false }
+let generatorReturnValue = iterator.next(3); // { value: 'hi', done: true }
+console.log(firstYield.value + "-" + secondYield.value + "-" + generatorReturnValue.value);
