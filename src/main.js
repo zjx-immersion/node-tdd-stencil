989b3f1b3ef866d83982a59d18d927ccd958ff9f
zjx-immersion <jianxin_home@163.com>
2017-03-17 15:14:02 +0800

    [Jason] refactor code to functional program step 1

--- src/main.js
+++ src/main.js
@@ -8,12 +8,30 @@ let numMap = {
 }
 let condition = (num, basicNum) => num % basicNum == 0;
 
+function action(num, num3, result, strFizz) {
+    return () => {
+        if (condition(num, num3)) {
+            result.push(strFizz)
+        }
+    }
+}
 function main(num) {
 
     let result = [];
-    if (condition(num, 3)) result.push('Fizz')
-    if (condition(num, 5)) result.push('Buzz')
-    if (condition(num, 7)) result.push('Whizz')
+    let num3 = 3;
+    let strFizz = 'Fizz';
+    let num5 = 5;
+    let strBuzz = 'Buzz';
+    let num7 = 7;
+    let strWhizz = 'Whizz';
+
+    r1 = action(num, num3, result, strFizz);
+    r2 = action(num, num5, result, strBuzz);
+    r3 = action(num, num7, result, strWhizz);
+
+    r1()
+    r2()
+    r3()
 
     if (result.length == 0) result.push(num.toString())
 
