6043f2ca10c80c1c5637ec44005e7a354de28c76
zjx-immersion <jianxin_home@163.com>
2017-03-17 14:48:12 +0800

    [Jason] tdd v1 for this practice

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 "use strick";
 
-function main() {
-    let temp = 'Hello World';
-    return temp;
+let numMap = {
+    3: 'Fizz',
+    5: 'Buzz',
+    7: 'Whizz',
+
+}
+let condition = (num, basicNum) => num % basicNum == 0;
+
+function main(num) {
+
+    let result = [];
+    if (condition(num, 3)) result.push('Fizz')
+    if (condition(num, 5)) result.push('Buzz')
+    if (condition(num, 7)) result.push('Whizz')
+
+    if (result.length == 0) result.push(num.toString())
+
+    return result.join('');
 }
 
 module.exports = main;
--- test/main_spec.js
+++ test/main_spec.js
@@ -9,24 +9,71 @@ chai.use(sinonChai);
 var main = require("../src/main.js");
 
 
-describe("测试描述", function() {
+describe("FizzBuzzWhizz", function () {
     //sinon.spy(console, 'log');
 
-    it("测试用例1", function() {
 
-        var result = main();
-        var expect_string = 'Hello World';
+    let constant = 2;
+    let num3 = 3;
+    let num5 = 5;
+    let num7 = 7;
+    let result_Fizz = 'Fizz';
+    let result_Buzz = 'Buzz';
+    let result_Whizz = 'Whizz';
+
+
+    it("input 2 return 2", function () {
+
+        var result = main(constant);
+
+        expect(constant.toString()).to.equal(result);
+    });
+
+    it("input 3 return Fizz", function () {
+        let result = main(num3);
+        expect(result_Fizz).to.equal(result);
+    });
+
+    it("input 5 return Buzz", function () {
+        let result = main(num5);
+        expect(result_Buzz).to.equal(result);
+    });
+
+    it("input 7 return Whizz", function () {
+        let result = main(num7);
+        expect(result_Whizz).to.equal(result);
+    });
+
+    it("input 3 * 5 return FizzBuzz", function () {
+
+        var result = main(num3 * num5);
+        var expect_string = `${result_Fizz}${result_Buzz}`;
 
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function() {
+    it("input 3 * 7 return FizzWhizz", function () {
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+        var result = main(num3 * num7);
+        var expect_string = `${result_Fizz}${result_Whizz}`;
 
-        console.log("result: " + result);
         expect(expect_string).to.equal(result);
     });
+
+    it("input 5 * 7 return BuzzWhizz", function () {
+
+        var result = main(num5 * num7);
+        var expect_string = `${result_Buzz}${result_Whizz}`;
+
+        expect(expect_string).to.equal(result);
+    });
+
+    it("input 3 * 5 * 7 return FizzBuzzWhizz", function () {
+
+        var result = main(num3 * num5 * num7);
+        var expect_string = `${result_Fizz}${result_Buzz}${result_Whizz}`;
+
+        expect(expect_string).to.equal(result);
+    });
+
 });
